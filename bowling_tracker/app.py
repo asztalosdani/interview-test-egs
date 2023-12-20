1955251fcfb1ea7c0e93bae46b554f271be829e1
asztalosdani <asztalosdani@gmail.com>
2023-12-21 00:30:17 +0100

    move to cell based view

--- bowling_tracker/app.py
+++ bowling_tracker/app.py
@@ -12,6 +12,9 @@ BACKGROUND_COLOR = QColor("#E2E8F8")
 
 class BowlingScoreTableModel(QAbstractTableModel):
 
+    FRAME_NUMBER_ROW = 0
+    SHOTS_ROW = 1
+
     def __init__(self, parent, bowling_score_tracker: BowlingScoreTracker):
         super().__init__(parent)
         self._bowling_score_tracker = bowling_score_tracker
@@ -20,47 +23,46 @@ class BowlingScoreTableModel(QAbstractTableModel):
         self.update()
 
     def rowCount(self, parent=...):
-        return 2
+        return 3
 
     def columnCount(self, parent=...):
-        return 10
+        return 21
 
     def data(self, index, role=...):
         row = index.row()
         column = index.column()
+        frame = int(column / 2) if column < self.columnCount() - 1 else 9
+        shot = column % 2 if column < self.columnCount() - 1 else 2
 
         if role == Qt.DisplayRole:
-            if row == 0:
-                if column > len(self._frames) - 1:
+            if row == self.FRAME_NUMBER_ROW:
+                return frame + 1
+            elif row == self.SHOTS_ROW:
+                if frame > len(self._frames) - 1:
                     return ""
 
-                shots = self._frames[column]
-                if len(shots) == 1 and shots[0] is Shot.STRIKE:
-                    return f"{shots[0].value} | _"
-                else:
-                    return " | ".join(str(shot.value) for shot in shots)
+                shots = self._frames[frame]
+                if shot < len(shots):
+                    return shots[shot].value
+                elif shots[0] is Shot.STRIKE and frame != 9:
+                    return "_"
             else:
 
-                if column > len(self._scores) - 1:
+                if frame > len(self._scores) - 1:
                     return None
 
-                return self._scores[column]
+                return self._scores[frame]
 
         if role == Qt.ItemDataRole.TextAlignmentRole:
             return Qt.AlignmentFlag.AlignCenter
 
         if role == Qt.ItemDataRole.BackgroundRole:
-            if row == 0:
-                if column == self._bowling_score_tracker.current_frame:
+            if row == self.SHOTS_ROW:
+                current_column = self._bowling_score_tracker.current_frame * 2 + self._bowling_score_tracker.current_shot
+                if not self._bowling_score_tracker.game_over and column == current_column:
                     return HIGHLIGHT_COLOR
                 else:
                     return BACKGROUND_COLOR
-
-    def headerData(self, section, orientation, role=...):
-        if orientation == Qt.Orientation.Horizontal:
-            return super().headerData(section, orientation, role)
-        else:
-            return None
 
     def update(self):
         self.beginResetModel()
@@ -75,7 +77,7 @@ class BowlingScoreTrackerDialog(QDialog):
         super().__init__()
 
         self.setWindowTitle("Bowling Score Tracker")
-        self.setFixedSize(500, 130)
+        self.setFixedSize(1000, 200)
         self.grabKeyboard()
 
         self._bowling_score_tracker = BowlingScoreTracker()
@@ -88,7 +90,14 @@ class BowlingScoreTrackerDialog(QDialog):
         self._table_view.setModel(self._table_model)
         self._table_view.setSelectionMode(QAbstractItemView.SelectionMode.NoSelection)
         self._table_view.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self._table_view.horizontalHeader().hide()
         self._table_view.verticalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self._table_view.verticalHeader().hide()
+        for i in range(9):
+            self._table_view.setSpan(0, i*2, 1, 2)
+            self._table_view.setSpan(2, i*2, 1, 2)
+        self._table_view.setSpan(0, 18, 1, 3)
+        self._table_view.setSpan(2, 18, 1, 3)
 
         button_layout = QHBoxLayout(self)
         main_layout.addLayout(button_layout)
